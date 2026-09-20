d7bd7274329a766663c623109a8a0e2ab7855342
Pierre Kostantine <150958805+pKostantine@users.noreply.github.com>
2026-09-20 00:10:09 -0400

    Return top-level book sections to Books

--- src/app/liturgy/index.tsx
+++ src/app/liturgy/index.tsx
@@ -2,5 +2,5 @@ import ServiceSubmenu from '@/components/chc/screens/ServiceSubmenu';
 import { LITURGY_GROUPS } from '@/constants/manifest';
 
 export default function LiturgySubmenu() {
-  return <ServiceSubmenu basePath="liturgy" title="Liturgy" arabic="القداس" services={LITURGY_GROUPS} backHref="/" />;
+  return <ServiceSubmenu basePath="liturgy" title="Liturgy" arabic="القداس" services={LITURGY_GROUPS} backHref="/books" />;
 }
