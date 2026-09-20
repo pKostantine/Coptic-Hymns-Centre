c4f104491ab421d2d2093010067c0de250b3ebc8
Pierre Kostantine <150958805+pKostantine@users.noreply.github.com>
2026-09-20 00:10:06 -0400

    Return top-level book sections to Books

--- src/app/agpeya/index.tsx
+++ src/app/agpeya/index.tsx
@@ -2,5 +2,5 @@ import ServiceSubmenu from '@/components/chc/screens/ServiceSubmenu';
 import { SERVICES_BY_CATEGORY } from '@/constants/manifest';
 
 export default function AgpeyaSubmenu() {
-  return <ServiceSubmenu basePath="agpeya" title="Agpeya" arabic="الأجبية" services={SERVICES_BY_CATEGORY.agpeya} backHref="/" />;
+  return <ServiceSubmenu basePath="agpeya" title="Agpeya" arabic="الأجبية" services={SERVICES_BY_CATEGORY.agpeya} backHref="/books" />;
 }
