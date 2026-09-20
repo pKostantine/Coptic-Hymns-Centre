5d2baef12d5c3c0fcfe59242bd935a1da391c2f8
Pierre Kostantine <150958805+pKostantine@users.noreply.github.com>
2026-09-20 00:10:14 -0400

    Return top-level book sections to Books

--- src/app/veneration/index.tsx
+++ src/app/veneration/index.tsx
@@ -1,5 +1,5 @@
 import ServiceDocument from '@/components/chc/screens/ServiceDocument';
 
 export default function VenerationDocument() {
-  return <ServiceDocument schema="veneration" table="veneration" title="Veneration" arabic="تمجيد" backHref="/" />;
+  return <ServiceDocument schema="veneration" table="veneration" title="Veneration" arabic="تمجيد" backHref="/books" />;
 }
